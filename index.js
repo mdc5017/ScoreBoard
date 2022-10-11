c03a269dff602c85fb1138f7a2dca7762982b406
María de la Paz Cardona <mpazcardona@gmail.com>
2022-10-11 15:42:11 +0100

    add goal and card class

---
+++ index.js
@@ -0,0 +1,43 @@
+teamAGoals = [];
+teamBGoals = [];
+
+teamACards = [];
+teamBCards = [];
+
+class Goal{
+    constructor(team, player, time){
+        this.team = team;
+        this.player = player;
+        this.time = time;
+    }
+
+    addGoal(){
+        if (this.team == "TeamA"){
+            teamAGoals.push(this);
+        }
+
+        else{
+            teamBGoals.push(this);
+        }
+    }
+    
+}
+
+class Card{
+    constructor(color, team, player, time){
+        this.color = color;
+        this.team = team;
+        this.player = player;
+        this.time = time;
+    }
+
+    addCard(){
+        if (this.team == "TeamA"){
+            teamACards.push(this);
+        }
+
+        else{
+            teamBCards.push(this);
+        }
+    }
+}
